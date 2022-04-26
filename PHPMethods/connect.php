99b390958d6e38f8052ec5e0297d363c6db81cc5
sniegocki <makssniegocki@gmail.com>
2022-04-26 13:09:40 +0200

    init

--- PHPMethods/connect.php
+++ PHPMethods/connect.php
@@ -1,5 +1,14 @@
 <?php
 
-    $connect = @new mysqli('localhost', 'user', 'haslo', 'nazwa_bazy');
+    $connect = @new mysqli('s110.linuxpl.com', 'nprofi15_santiego', '4g]N--hcf)-9MdGD', 'nprofi15_santiego');
+
+    try
+    {
+        require "connect.php";
+    }
+    catch(Exception $e)
+    {
+        echo "An error has occured: " . $e;
+    }
 
 ?>
