e21e2f7b2c5ebc560d72ce44f6b293be1846cdc3
Patryk Kowalski <pacislavek@gmail.com>
2022-06-04 20:58:17 +0200

    sql database language fix

--- PHPMethods/connect.php
+++ PHPMethods/connect.php
@@ -6,6 +6,7 @@
     try
     {
         $connect = @new mysqli('santiego.eu', 'nprofi15_santiego', '4g]N--hcf)-9MdGD', 'nprofi15_santiego');
+        mysqli_set_charset($connect, "utf8");
     }
     catch(Exception $e)
     {
