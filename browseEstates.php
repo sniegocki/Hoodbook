5f1be660b8f6e142d409e38d837a5d0dac04a399
Patryk Kowalski <pacislavek@gmail.com>
2022-06-04 18:27:24 +0200

    estate name fix

--- browseEstates.php
+++ browseEstates.php
@@ -83,7 +83,7 @@
                             echo ('
                                 <div class="col-12 col-sm-6 col-lg-4 col-xl-3">
                                     <div class="d-flex flex-column bg-white rounded shadow align-items-center p-3">
-                                        <h3 class="h3 text-center">'.$estateName.'</h3>
+                                        <h3 class="h3 text-center" style="overflow-wrap: anywhere;">'.$estateName.'</h3>
                                         <img src="img/estate-placeholder.svg" alt="zdjęcie osiedla" class="img-fluid">
                                         <p class="text-muted mt-2 mb-0">'.$estateStreet.', '.$estateZipCode.', '.$estateCity.' '.$estateCountry.'</p>
                                         <p class="text-muted mt-2">Liczba mieszkańców: '.$estateUsersCount.'</p>
