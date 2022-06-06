2eb5213dcc3d114b176f855504e2cc3ff9c18152
Patryk Kowalski <pacislavek@gmail.com>
2022-06-06 22:27:50 +0200

    final connect

--- PHPMethods/connect.php
+++ PHPMethods/connect.php
@@ -2,15 +2,28 @@
 
     //connection for all files
     $connect;
+    $connect = @mysqli_connect('santiego.eu', 'nprofi15_santiego', '4g]N--hcf)-9MdGD', 'nprofi15_santiego');
 
-    try
+    if($connect)
     {
-        $connect = @new mysqli('santiego.eu', 'nprofi15_santiego', '4g]N--hcf)-9MdGD', 'nprofi15_santiego');
+        //remote DB connection success
         mysqli_set_charset($connect, "utf8");
     }
-    catch(Exception $e)
+    else
     {
-        echo "An error has occured: " . $e;
+        //echo "Trying to connect to local database";
+        try
+        {
+            $connect = @new mysqli('localhost', 'nprofi15_santiego', '4g]N--hcf)-9MdGD', 'nprofi15_santiego');
+        }
+        catch(Exception $e)
+        {
+            echo "Local DB connection failed. Error:";
+            echo $e;
+        }
     }
 
+    mysqli_set_charset($connect, "utf8");
+
 ?>
+
